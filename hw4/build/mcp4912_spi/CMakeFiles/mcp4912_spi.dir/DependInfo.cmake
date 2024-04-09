ed2442accfeedb26f8c4ecc37b31eb24e1436e0e
LOLschansky <116858248+LOLschansky@users.noreply.github.com>
2024-04-09 00:37:49 -0500

    hw4

---
+++ hw4/build/mcp4912_spi/CMakeFiles/mcp4912_spi.dir/DependInfo.cmake
@@ -0,0 +1,158 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/hardware_divider/divider.S" "/Users/lucasolschansky/Desktop/meche433/hw4/build/mcp4912_spi/CMakeFiles/mcp4912_spi.dir/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/hardware_divider/divider.S.obj"
+  "/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/hardware_irq/irq_handler_chain.S" "/Users/lucasolschansky/Desktop/meche433/hw4/build/mcp4912_spi/CMakeFiles/mcp4912_spi.dir/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/hardware_irq/irq_handler_chain.S.obj"
+  "/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/pico_bit_ops/bit_ops_aeabi.S" "/Users/lucasolschansky/Desktop/meche433/hw4/build/mcp4912_spi/CMakeFiles/mcp4912_spi.dir/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/pico_bit_ops/bit_ops_aeabi.S.obj"
+  "/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/pico_divider/divider.S" "/Users/lucasolschansky/Desktop/meche433/hw4/build/mcp4912_spi/CMakeFiles/mcp4912_spi.dir/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/pico_divider/divider.S.obj"
+  "/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/pico_double/double_aeabi.S" "/Users/lucasolschansky/Desktop/meche433/hw4/build/mcp4912_spi/CMakeFiles/mcp4912_spi.dir/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/pico_double/double_aeabi.S.obj"
+  "/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/pico_double/double_v1_rom_shim.S" "/Users/lucasolschansky/Desktop/meche433/hw4/build/mcp4912_spi/CMakeFiles/mcp4912_spi.dir/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/pico_double/double_v1_rom_shim.S.obj"
+  "/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/pico_float/float_aeabi.S" "/Users/lucasolschansky/Desktop/meche433/hw4/build/mcp4912_spi/CMakeFiles/mcp4912_spi.dir/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/pico_float/float_aeabi.S.obj"
+  "/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/pico_float/float_v1_rom_shim.S" "/Users/lucasolschansky/Desktop/meche433/hw4/build/mcp4912_spi/CMakeFiles/mcp4912_spi.dir/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/pico_float/float_v1_rom_shim.S.obj"
+  "/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/pico_int64_ops/pico_int64_ops_aeabi.S" "/Users/lucasolschansky/Desktop/meche433/hw4/build/mcp4912_spi/CMakeFiles/mcp4912_spi.dir/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/pico_int64_ops/pico_int64_ops_aeabi.S.obj"
+  "/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/pico_mem_ops/mem_ops_aeabi.S" "/Users/lucasolschansky/Desktop/meche433/hw4/build/mcp4912_spi/CMakeFiles/mcp4912_spi.dir/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/pico_mem_ops/mem_ops_aeabi.S.obj"
+  "/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/pico_standard_link/crt0.S" "/Users/lucasolschansky/Desktop/meche433/hw4/build/mcp4912_spi/CMakeFiles/mcp4912_spi.dir/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/pico_standard_link/crt0.S.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "LIB_PICO_BIT_OPS=1"
+  "LIB_PICO_BIT_OPS_PICO=1"
+  "LIB_PICO_DIVIDER=1"
+  "LIB_PICO_DIVIDER_HARDWARE=1"
+  "LIB_PICO_DOUBLE=1"
+  "LIB_PICO_DOUBLE_PICO=1"
+  "LIB_PICO_FLOAT=1"
+  "LIB_PICO_FLOAT_PICO=1"
+  "LIB_PICO_INT64_OPS=1"
+  "LIB_PICO_INT64_OPS_PICO=1"
+  "LIB_PICO_MALLOC=1"
+  "LIB_PICO_MEM_OPS=1"
+  "LIB_PICO_MEM_OPS_PICO=1"
+  "LIB_PICO_PLATFORM=1"
+  "LIB_PICO_PRINTF=1"
+  "LIB_PICO_PRINTF_PICO=1"
+  "LIB_PICO_RUNTIME=1"
+  "LIB_PICO_STANDARD_LINK=1"
+  "LIB_PICO_STDIO=1"
+  "LIB_PICO_STDIO_UART=1"
+  "LIB_PICO_STDLIB=1"
+  "LIB_PICO_SYNC=1"
+  "LIB_PICO_SYNC_CRITICAL_SECTION=1"
+  "LIB_PICO_SYNC_MUTEX=1"
+  "LIB_PICO_SYNC_SEM=1"
+  "LIB_PICO_TIME=1"
+  "LIB_PICO_UTIL=1"
+  "PICO_BOARD=\"pico\""
+  "PICO_BUILD=1"
+  "PICO_CMAKE_BUILD_TYPE=\"Debug\""
+  "PICO_COPY_TO_RAM=0"
+  "PICO_CXX_ENABLE_EXCEPTIONS=0"
+  "PICO_NO_FLASH=0"
+  "PICO_NO_HARDWARE=0"
+  "PICO_ON_DEVICE=1"
+  "PICO_TARGET_NAME=\"mcp4912_spi\""
+  "PICO_USE_BLOCKED_RAM=0"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "/Users/lucasolschansky/Pico/pico-sdk/src/common/pico_stdlib/include"
+  "/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/hardware_gpio/include"
+  "/Users/lucasolschansky/Pico/pico-sdk/src/common/pico_base/include"
+  "generated/pico_base"
+  "/Users/lucasolschansky/Pico/pico-sdk/src/boards/include"
+  "/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/pico_platform/include"
+  "/Users/lucasolschansky/Pico/pico-sdk/src/rp2040/hardware_regs/include"
+  "/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/hardware_base/include"
+  "/Users/lucasolschansky/Pico/pico-sdk/src/rp2040/hardware_structs/include"
+  "/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/hardware_claim/include"
+  "/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/hardware_sync/include"
+  "/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/hardware_irq/include"
+  "/Users/lucasolschansky/Pico/pico-sdk/src/common/pico_sync/include"
+  "/Users/lucasolschansky/Pico/pico-sdk/src/common/pico_time/include"
+  "/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/hardware_timer/include"
+  "/Users/lucasolschansky/Pico/pico-sdk/src/common/pico_util/include"
+  "/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/hardware_uart/include"
+  "/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/hardware_resets/include"
+  "/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/hardware_clocks/include"
+  "/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/hardware_pll/include"
+  "/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/hardware_vreg/include"
+  "/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/hardware_watchdog/include"
+  "/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/hardware_xosc/include"
+  "/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/hardware_divider/include"
+  "/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/pico_runtime/include"
+  "/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/pico_printf/include"
+  "/Users/lucasolschansky/Pico/pico-sdk/src/common/pico_bit_ops/include"
+  "/Users/lucasolschansky/Pico/pico-sdk/src/common/pico_divider/include"
+  "/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/pico_double/include"
+  "/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/pico_float/include"
+  "/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/pico_malloc/include"
+  "/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/pico_bootrom/include"
+  "/Users/lucasolschansky/Pico/pico-sdk/src/common/pico_binary_info/include"
+  "/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/pico_stdio/include"
+  "/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/pico_stdio_uart/include"
+  "/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/pico_int64_ops/include"
+  "/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/pico_mem_ops/include"
+  "/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/boot_stage2/include"
+  "/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/hardware_spi/include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/lucasolschansky/Pico/pico-sdk/src/common/pico_sync/critical_section.c" "mcp4912_spi/CMakeFiles/mcp4912_spi.dir/Users/lucasolschansky/Pico/pico-sdk/src/common/pico_sync/critical_section.c.obj" "gcc" "mcp4912_spi/CMakeFiles/mcp4912_spi.dir/Users/lucasolschansky/Pico/pico-sdk/src/common/pico_sync/critical_section.c.obj.d"
+  "/Users/lucasolschansky/Pico/pico-sdk/src/common/pico_sync/lock_core.c" "mcp4912_spi/CMakeFiles/mcp4912_spi.dir/Users/lucasolschansky/Pico/pico-sdk/src/common/pico_sync/lock_core.c.obj" "gcc" "mcp4912_spi/CMakeFiles/mcp4912_spi.dir/Users/lucasolschansky/Pico/pico-sdk/src/common/pico_sync/lock_core.c.obj.d"
+  "/Users/lucasolschansky/Pico/pico-sdk/src/common/pico_sync/mutex.c" "mcp4912_spi/CMakeFiles/mcp4912_spi.dir/Users/lucasolschansky/Pico/pico-sdk/src/common/pico_sync/mutex.c.obj" "gcc" "mcp4912_spi/CMakeFiles/mcp4912_spi.dir/Users/lucasolschansky/Pico/pico-sdk/src/common/pico_sync/mutex.c.obj.d"
+  "/Users/lucasolschansky/Pico/pico-sdk/src/common/pico_sync/sem.c" "mcp4912_spi/CMakeFiles/mcp4912_spi.dir/Users/lucasolschansky/Pico/pico-sdk/src/common/pico_sync/sem.c.obj" "gcc" "mcp4912_spi/CMakeFiles/mcp4912_spi.dir/Users/lucasolschansky/Pico/pico-sdk/src/common/pico_sync/sem.c.obj.d"
+  "/Users/lucasolschansky/Pico/pico-sdk/src/common/pico_time/time.c" "mcp4912_spi/CMakeFiles/mcp4912_spi.dir/Users/lucasolschansky/Pico/pico-sdk/src/common/pico_time/time.c.obj" "gcc" "mcp4912_spi/CMakeFiles/mcp4912_spi.dir/Users/lucasolschansky/Pico/pico-sdk/src/common/pico_time/time.c.obj.d"
+  "/Users/lucasolschansky/Pico/pico-sdk/src/common/pico_time/timeout_helper.c" "mcp4912_spi/CMakeFiles/mcp4912_spi.dir/Users/lucasolschansky/Pico/pico-sdk/src/common/pico_time/timeout_helper.c.obj" "gcc" "mcp4912_spi/CMakeFiles/mcp4912_spi.dir/Users/lucasolschansky/Pico/pico-sdk/src/common/pico_time/timeout_helper.c.obj.d"
+  "/Users/lucasolschansky/Pico/pico-sdk/src/common/pico_util/datetime.c" "mcp4912_spi/CMakeFiles/mcp4912_spi.dir/Users/lucasolschansky/Pico/pico-sdk/src/common/pico_util/datetime.c.obj" "gcc" "mcp4912_spi/CMakeFiles/mcp4912_spi.dir/Users/lucasolschansky/Pico/pico-sdk/src/common/pico_util/datetime.c.obj.d"
+  "/Users/lucasolschansky/Pico/pico-sdk/src/common/pico_util/pheap.c" "mcp4912_spi/CMakeFiles/mcp4912_spi.dir/Users/lucasolschansky/Pico/pico-sdk/src/common/pico_util/pheap.c.obj" "gcc" "mcp4912_spi/CMakeFiles/mcp4912_spi.dir/Users/lucasolschansky/Pico/pico-sdk/src/common/pico_util/pheap.c.obj.d"
+  "/Users/lucasolschansky/Pico/pico-sdk/src/common/pico_util/queue.c" "mcp4912_spi/CMakeFiles/mcp4912_spi.dir/Users/lucasolschansky/Pico/pico-sdk/src/common/pico_util/queue.c.obj" "gcc" "mcp4912_spi/CMakeFiles/mcp4912_spi.dir/Users/lucasolschansky/Pico/pico-sdk/src/common/pico_util/queue.c.obj.d"
+  "/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/hardware_claim/claim.c" "mcp4912_spi/CMakeFiles/mcp4912_spi.dir/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/hardware_claim/claim.c.obj" "gcc" "mcp4912_spi/CMakeFiles/mcp4912_spi.dir/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/hardware_claim/claim.c.obj.d"
+  "/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/hardware_clocks/clocks.c" "mcp4912_spi/CMakeFiles/mcp4912_spi.dir/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/hardware_clocks/clocks.c.obj" "gcc" "mcp4912_spi/CMakeFiles/mcp4912_spi.dir/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/hardware_clocks/clocks.c.obj.d"
+  "/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/hardware_gpio/gpio.c" "mcp4912_spi/CMakeFiles/mcp4912_spi.dir/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/hardware_gpio/gpio.c.obj" "gcc" "mcp4912_spi/CMakeFiles/mcp4912_spi.dir/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/hardware_gpio/gpio.c.obj.d"
+  "/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/hardware_irq/irq.c" "mcp4912_spi/CMakeFiles/mcp4912_spi.dir/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/hardware_irq/irq.c.obj" "gcc" "mcp4912_spi/CMakeFiles/mcp4912_spi.dir/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/hardware_irq/irq.c.obj.d"
+  "/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/hardware_pll/pll.c" "mcp4912_spi/CMakeFiles/mcp4912_spi.dir/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/hardware_pll/pll.c.obj" "gcc" "mcp4912_spi/CMakeFiles/mcp4912_spi.dir/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/hardware_pll/pll.c.obj.d"
+  "/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/hardware_spi/spi.c" "mcp4912_spi/CMakeFiles/mcp4912_spi.dir/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/hardware_spi/spi.c.obj" "gcc" "mcp4912_spi/CMakeFiles/mcp4912_spi.dir/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/hardware_spi/spi.c.obj.d"
+  "/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/hardware_sync/sync.c" "mcp4912_spi/CMakeFiles/mcp4912_spi.dir/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/hardware_sync/sync.c.obj" "gcc" "mcp4912_spi/CMakeFiles/mcp4912_spi.dir/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/hardware_sync/sync.c.obj.d"
+  "/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/hardware_timer/timer.c" "mcp4912_spi/CMakeFiles/mcp4912_spi.dir/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/hardware_timer/timer.c.obj" "gcc" "mcp4912_spi/CMakeFiles/mcp4912_spi.dir/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/hardware_timer/timer.c.obj.d"
+  "/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/hardware_uart/uart.c" "mcp4912_spi/CMakeFiles/mcp4912_spi.dir/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/hardware_uart/uart.c.obj" "gcc" "mcp4912_spi/CMakeFiles/mcp4912_spi.dir/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/hardware_uart/uart.c.obj.d"
+  "/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/hardware_vreg/vreg.c" "mcp4912_spi/CMakeFiles/mcp4912_spi.dir/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/hardware_vreg/vreg.c.obj" "gcc" "mcp4912_spi/CMakeFiles/mcp4912_spi.dir/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/hardware_vreg/vreg.c.obj.d"
+  "/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/hardware_watchdog/watchdog.c" "mcp4912_spi/CMakeFiles/mcp4912_spi.dir/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/hardware_watchdog/watchdog.c.obj" "gcc" "mcp4912_spi/CMakeFiles/mcp4912_spi.dir/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/hardware_watchdog/watchdog.c.obj.d"
+  "/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/hardware_xosc/xosc.c" "mcp4912_spi/CMakeFiles/mcp4912_spi.dir/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/hardware_xosc/xosc.c.obj" "gcc" "mcp4912_spi/CMakeFiles/mcp4912_spi.dir/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/hardware_xosc/xosc.c.obj.d"
+  "/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/pico_bootrom/bootrom.c" "mcp4912_spi/CMakeFiles/mcp4912_spi.dir/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/pico_bootrom/bootrom.c.obj" "gcc" "mcp4912_spi/CMakeFiles/mcp4912_spi.dir/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/pico_bootrom/bootrom.c.obj.d"
+  "/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/pico_double/double_init_rom.c" "mcp4912_spi/CMakeFiles/mcp4912_spi.dir/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/pico_double/double_init_rom.c.obj" "gcc" "mcp4912_spi/CMakeFiles/mcp4912_spi.dir/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/pico_double/double_init_rom.c.obj.d"
+  "/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/pico_double/double_math.c" "mcp4912_spi/CMakeFiles/mcp4912_spi.dir/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/pico_double/double_math.c.obj" "gcc" "mcp4912_spi/CMakeFiles/mcp4912_spi.dir/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/pico_double/double_math.c.obj.d"
+  "/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/pico_float/float_init_rom.c" "mcp4912_spi/CMakeFiles/mcp4912_spi.dir/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/pico_float/float_init_rom.c.obj" "gcc" "mcp4912_spi/CMakeFiles/mcp4912_spi.dir/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/pico_float/float_init_rom.c.obj.d"
+  "/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/pico_float/float_math.c" "mcp4912_spi/CMakeFiles/mcp4912_spi.dir/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/pico_float/float_math.c.obj" "gcc" "mcp4912_spi/CMakeFiles/mcp4912_spi.dir/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/pico_float/float_math.c.obj.d"
+  "/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/pico_malloc/pico_malloc.c" "mcp4912_spi/CMakeFiles/mcp4912_spi.dir/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/pico_malloc/pico_malloc.c.obj" "gcc" "mcp4912_spi/CMakeFiles/mcp4912_spi.dir/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/pico_malloc/pico_malloc.c.obj.d"
+  "/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/pico_platform/platform.c" "mcp4912_spi/CMakeFiles/mcp4912_spi.dir/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/pico_platform/platform.c.obj" "gcc" "mcp4912_spi/CMakeFiles/mcp4912_spi.dir/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/pico_platform/platform.c.obj.d"
+  "/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/pico_printf/printf.c" "mcp4912_spi/CMakeFiles/mcp4912_spi.dir/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/pico_printf/printf.c.obj" "gcc" "mcp4912_spi/CMakeFiles/mcp4912_spi.dir/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/pico_printf/printf.c.obj.d"
+  "/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/pico_runtime/runtime.c" "mcp4912_spi/CMakeFiles/mcp4912_spi.dir/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/pico_runtime/runtime.c.obj" "gcc" "mcp4912_spi/CMakeFiles/mcp4912_spi.dir/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/pico_runtime/runtime.c.obj.d"
+  "/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/pico_standard_link/binary_info.c" "mcp4912_spi/CMakeFiles/mcp4912_spi.dir/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/pico_standard_link/binary_info.c.obj" "gcc" "mcp4912_spi/CMakeFiles/mcp4912_spi.dir/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/pico_standard_link/binary_info.c.obj.d"
+  "/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/pico_stdio/stdio.c" "mcp4912_spi/CMakeFiles/mcp4912_spi.dir/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/pico_stdio/stdio.c.obj" "gcc" "mcp4912_spi/CMakeFiles/mcp4912_spi.dir/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/pico_stdio/stdio.c.obj.d"
+  "/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/pico_stdio_uart/stdio_uart.c" "mcp4912_spi/CMakeFiles/mcp4912_spi.dir/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/pico_stdio_uart/stdio_uart.c.obj" "gcc" "mcp4912_spi/CMakeFiles/mcp4912_spi.dir/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/pico_stdio_uart/stdio_uart.c.obj.d"
+  "/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/pico_stdlib/stdlib.c" "mcp4912_spi/CMakeFiles/mcp4912_spi.dir/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/pico_stdlib/stdlib.c.obj" "gcc" "mcp4912_spi/CMakeFiles/mcp4912_spi.dir/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/pico_stdlib/stdlib.c.obj.d"
+  "/Users/lucasolschansky/Desktop/meche433/hw4/mcp4912_spi/mcp4912_spi.c" "mcp4912_spi/CMakeFiles/mcp4912_spi.dir/mcp4912_spi.c.obj" "gcc" "mcp4912_spi/CMakeFiles/mcp4912_spi.dir/mcp4912_spi.c.obj.d"
+  "/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/pico_standard_link/new_delete.cpp" "mcp4912_spi/CMakeFiles/mcp4912_spi.dir/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/pico_standard_link/new_delete.cpp.obj" "gcc" "mcp4912_spi/CMakeFiles/mcp4912_spi.dir/Users/lucasolschansky/Pico/pico-sdk/src/rp2_common/pico_standard_link/new_delete.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ hw4/mcp4912_spi/mcp4912_spi.c
@@ -0,0 +1,106 @@
+/**
+ * Copyright (c) 2020 Raspberry Pi (Trading) Ltd.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include "pico/stdlib.h"
+#include "pico/binary_info.h"
+#include "hardware/spi.h"
+#include "math.h"
+
+
+#define READ_BIT 0x80
+
+int32_t t_fine;
+
+uint16_t dig_T1;
+int16_t dig_T2, dig_T3;
+uint16_t dig_P1;
+int16_t dig_P2, dig_P3, dig_P4, dig_P5, dig_P6, dig_P7, dig_P8, dig_P9;
+uint8_t dig_H1, dig_H3;
+int8_t dig_H6;
+int16_t dig_H2, dig_H4, dig_H5;
+
+
+
+#ifdef PICO_DEFAULT_SPI_CSN_PIN
+static inline void cs_select() {
+    asm volatile("nop \n nop \n nop");
+    gpio_put(PICO_DEFAULT_SPI_CSN_PIN, 0);  // Active low
+    asm volatile("nop \n nop \n nop");
+}
+
+static inline void cs_deselect() {
+    asm volatile("nop \n nop \n nop");
+    gpio_put(PICO_DEFAULT_SPI_CSN_PIN, 1);
+    asm volatile("nop \n nop \n nop");
+}
+#endif
+
+#if defined(spi_default) && defined(PICO_DEFAULT_SPI_CSN_PIN)
+
+static void write_register(unsigned short voltage, int channel) {
+    uint8_t buf[2];
+    
+    //buf[0] = 0b01111111;
+    channel = channel << 7;
+    buf[0] = channel;
+    buf[0] = buf[0] | (0b111 << 4);
+    buf[0] = buf[0] | (voltage >> 6);
+
+    buf[1] = (voltage >> 4) << 2;
+    buf[1] = buf[1] | 0b00000011; // last two bits can be anything
+
+    cs_select();
+    spi_write_blocking(spi_default, buf, 2);
+    cs_deselect();
+
+
+}
+#endif
+
+
+int main() {
+    stdio_init_all();
+#if !defined(spi_default) || !defined(PICO_DEFAULT_SPI_SCK_PIN) || !defined(PICO_DEFAULT_SPI_TX_PIN) || !defined(PICO_DEFAULT_SPI_RX_PIN) || !defined(PICO_DEFAULT_SPI_CSN_PIN)
+#warning spi/bme280_spi example requires a board with SPI pins
+    puts("Default SPI pins were not defined");
+#else
+
+    // Set baud to 1MHhz
+    spi_init(spi_default, 1000 * 1000);
+    gpio_set_function(PICO_DEFAULT_SPI_RX_PIN, GPIO_FUNC_SPI);
+    gpio_set_function(PICO_DEFAULT_SPI_SCK_PIN, GPIO_FUNC_SPI);
+    gpio_set_function(PICO_DEFAULT_SPI_TX_PIN, GPIO_FUNC_SPI);
+    // Make the SPI pins available to picotool
+    bi_decl(bi_3pins_with_func(PICO_DEFAULT_SPI_RX_PIN, PICO_DEFAULT_SPI_TX_PIN, PICO_DEFAULT_SPI_SCK_PIN, GPIO_FUNC_SPI));
+
+    // Chip select is active-low, so we'll initialise it to a driven-high state
+    gpio_init(PICO_DEFAULT_SPI_CSN_PIN);
+    gpio_set_dir(PICO_DEFAULT_SPI_CSN_PIN, GPIO_OUT);
+    gpio_put(PICO_DEFAULT_SPI_CSN_PIN, 1);
+    // Make the CS pin available to picotool
+    bi_decl(bi_1pin_with_name(PICO_DEFAULT_SPI_CSN_PIN, "SPI CS"));
+
+    int rate = 1;
+    int t = 0;
+
+       while (true){
+        float voltage_fl_sin = sin(2.0*3.1415*2*(((float) t)/1024.0))*511.0 + 511.0;
+        int voltage_sin = (int) voltage_fl_sin;
+
+        int voltage_triangle = t + 511;
+        write_register(voltage_triangle, 1);
+        write_register(voltage_sin, 0);
+        if ((t == 0) || (t == 1024)){
+            rate = rate * -1;
+        }
+        sleep_ms(1);
+        t = t + rate;
+
+    }
+#endif
+}
